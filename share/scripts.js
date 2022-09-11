969ce441b07a4cb726b4265504c64cc6dee05676
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2022-09-11 11:54:05 +0100

    Added website share links

---
+++ share/scripts.js
@@ -0,0 +1,14 @@
+window.onload = async function() {
+	let id = location.search.split("?q=")[1];
+	if(!id) { return window.location = "../index.html"}
+	
+	$.getJSON(`http://de1.api.radio-browser.info/json/stations/byuuid/${id}`, function(data) {
+		console.log(data)
+		document.getElementById('name').innerText = data[0] ? data[0].name : "We couldn't match that id to a station"
+		document.getElementById('desc').innerText = data[0] ? `Tags: ${data[0].tags.replace(',', ', ')}` : "We can't provide details for this station in a preview here. You can try opening it in-app anyway, but we don't think it'll work.";
+		document.getElementById('buttonContent').innerText = data[0] ? `Open ${data[0].name}` : `Open anyway`;
+		document.getElementById('site').href = data[0] ? data[0].homepage : "../"
+		document.getElementById('thingymabob').href = "getradio://" + id;
+		//document.getElementById('')
+	})
+}
