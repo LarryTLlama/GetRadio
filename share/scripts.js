ab74c0ab2658fb79c85147e65eb118d64f224da3
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2022-09-11 11:56:06 +0100

    Update scripts.js

--- share/scripts.js
+++ share/scripts.js
@@ -1,8 +1,8 @@
 window.onload = async function() {
 	let id = location.search.split("?q=")[1];
-	if(!id) { return window.location = "../index.html"}
+	if(!id) { return window.location = "../"}
 	
-	$.getJSON(`http://de1.api.radio-browser.info/json/stations/byuuid/${id}`, function(data) {
+	$.getJSON(`https://de1.api.radio-browser.info/json/stations/byuuid/${id}`, function(data) {
 		console.log(data)
 		document.getElementById('name').innerText = data[0] ? data[0].name : "We couldn't match that id to a station"
 		document.getElementById('desc').innerText = data[0] ? `Tags: ${data[0].tags.replace(',', ', ')}` : "We can't provide details for this station in a preview here. You can try opening it in-app anyway, but we don't think it'll work.";
